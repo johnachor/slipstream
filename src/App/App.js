d371ef61f85b7884af055a97a7b92dfeaca2f25e
John Achor <johnachor@gmail.com>
2018-07-16 09:33:30 -0500

    updated packages, moved justwatch tests out of App.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-const JustWatch = require('justwatch-api');
-
-const jw = new JustWatch();
-
-jw.getProviders().then(providers => console.log('providers:', providers));
-jw.search('Anna Kendrick').then(results => console.log('actor search:', results.items));
-jw.search('Deadpool').then(results => console.log('title search:', results.items));
-jw.getTitle('movie', 123777).then(movie => console.log('retrieve specific movie:', movie));
-jw.getPerson(6716).then(person => console.log('retrieve specific person:', person));
-jw.getGenres().then(genres => console.log('genres:', genres));
 
 class App extends Component {
   render() {
