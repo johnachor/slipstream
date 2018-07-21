cb5151b3982260ac676d1b166f15e5be9da0f80f
John Achor <johnachor@gmail.com>
2018-07-21 00:55:44 -0500

    tentative structure for UD on queue item component invocation

--- src/components/Queue/Queue.js
+++ src/components/Queue/Queue.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import fbQueue from '../../firebaseReqs/queue';
+import QueuedMedia from '../QueuedMedia/QueuedMedia';
 import './Queue.css';
 
 class Queue extends React.Component {
@@ -8,12 +9,14 @@ class Queue extends React.Component {
     queue: [],
   }
 
-  componentDidMount() {
+  updateQueue = () => {
     fbQueue.getMyQueue()
       .then(results => {
         const queuedItems = Object.entries(results.data).reduce((queueArray, queuedItem) => {
-          queuedItem[1].firebaseId = queuedItem[0];
-          queueArray.push(queuedItem[1]);
+          if (queuedItem[1].isReviewed === false) {
+            queuedItem[1].firebaseId = queuedItem[0];
+            queueArray.push(queuedItem[1]);
+          }
           return queueArray;
         }, []);
         const tempState = { ...this.state };
@@ -23,7 +26,18 @@ class Queue extends React.Component {
       .catch(err => console.error(err));
   }
 
+  componentDidMount() {
+    this.updateQueue();
+  }
+
   render() {
+
+    const queueCards = this.state.queue.map(queueItem => {
+      return (
+        <QueuedMedia key={queueItem.firebaseId} onReviewButtonClick={this.reviewQueuedItem} onDeleteButtonClick={this.deleteQueuedItem} media={queueItem} />
+      );
+    });
+
     return (
       <div className="Queue">
         <h1>Queue</h1>
