d7e6a0343fc26b3e31dde003ce6bfe86e819196a
John Achor <johnachor@gmail.com>
2018-07-22 20:46:43 -0500

    activity feed state now includes array of friends' reviews sorted by date

--- src/components/ActivityFeed/ActivityFeed.js
+++ src/components/ActivityFeed/ActivityFeed.js
@@ -1,8 +1,37 @@
 import React from 'react';
-
+import fbReviews from '../../firebaseReqs/queue';
 import './ActivityFeed.css';
 
 class ActivityFeed extends React.Component {
+
+  state = {
+    friendReviews: [],
+  }
+
+  getFriendActivities = () => {
+    fbReviews.getEntireQueue()
+      .then(queueItems => {
+        const friendReviews = Object.entries(queueItems.data)
+          .reduce((acc, kvp) => {
+            kvp[1].firebaseId = kvp[0];
+            acc.push(kvp[1]);
+            return acc;
+          }, [])
+          .filter(review => {
+            return this.props.friendUids.includes(review.ownerUid) && review.isReviewed === true;
+          })
+          .sort((a, b) => {
+            return a.reviewDate - b.reviewDate;
+          });
+        this.setState({ friendReviews: friendReviews });
+      })
+      .catch(err => console.error(err));
+  }
+
+  componentDidMount() {
+    this.getFriendActivities();
+  }
+
   render() {
     return (
       <div className="ActivityFeed">
