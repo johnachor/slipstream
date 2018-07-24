c05ae10e75b86410b97fcf33bd3a8655b25f226c
John Achor <johnachor@gmail.com>
2018-07-23 21:11:43 -0500

    replaced api module with call to heroku proxy

--- src/justwatchApi/justwatch.js
+++ src/justwatchApi/justwatch.js
@@ -1,10 +1,5 @@
-// The below imported module is included as-is in the project source so it will build with the
-// unmodified create-react-app build script. Only the export line has been changed.
-// TODO when project is complete and maintained: fork and refactor this module for ES6/front-end
-import JustWatch from './original-package/justwatch-api/index';
 
-const jw = new JustWatch();
-
+import axios from 'axios';
 // jw.getProviders().then(providers => console.log('providers:', providers));
 // jw.search('Anna Kendrick').then(results => console.log('actor search:', results.items));
 // jw.search('Deadpool').then(results => console.log('title search:', results.items));
@@ -13,7 +8,7 @@ const jw = new JustWatch();
 // jw.getGenres().then(genres => console.log('genres:', genres));
 
 const jwSearch = (searchTerm) => {
-  return jw.search(searchTerm);
+  return axios.post('https://murmuring-spire-99787.herokuapp.com/search', { searchString: searchTerm });
 };
 
 export default { jwSearch };
