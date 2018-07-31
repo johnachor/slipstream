fcf79db046329f10db432d6ac7eeafd918bfe5cb
John Achor <johnachor@gmail.com>
2018-07-31 10:09:24 -0500

    navbar replaced with correctly working react-bootstrap components

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, NavItem, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 import fbAuth from '../../firebaseReqs/auth';
 import './NavBar.css';
 import Logo from './img/logo.png';
@@ -12,26 +12,44 @@ class NavBar extends React.Component {
 
     const inLinksLeft = (
       <Nav>
-        <NavItem eventKey={1}>
-          <Link to="/dashboard">Dashboard</Link>
-        </NavItem>
-        <NavItem eventKey={2}>
-          <Link to="/queue">My Queue</Link>
-        </NavItem>
-        <NavItem eventKey={3}>
-          <Link to="/myreviews">My Reviews</Link>
-        </NavItem>
-        <NavItem eventKey={4}>
-          <Link to="/search">Search</Link>
+        <LinkContainer to="/dashboard">
+          <NavItem eventKey={1}>
+          Dashboard
+          </NavItem>
+        </LinkContainer>
+        <LinkContainer to="/queue">
+          <NavItem eventKey={2}>
+            My Queue
+          </NavItem>
+        </LinkContainer>
+        <LinkContainer to="/myreviews">
+          <NavItem eventKey={3}>
+            My Reviews
+          </NavItem>
+        </LinkContainer>
+        <LinkContainer to="/search">
+          <NavItem eventKey={4}>
+            Search
+          </NavItem>
+        </LinkContainer>
+      </Nav>
+    );
+
+    const logoutLink = (
+      <Nav pullRight>
+        <NavItem eventKey={5}>
+          <Button className="authButton" bsStyle="link" onClick={fbAuth.logoutUser}>Logout</Button>
         </NavItem>
       </Nav>
     );
 
-    const inLinksRight = (
+    const loginLink = (
       <Nav pullRight>
-        <NavItem eventKey={5}>
-          <Button bsStyle="warning" onClick={fbAuth.logoutUser}>Logout</Button>
-        </NavItem>
+        <LinkContainer to="/login">
+          <NavItem eventKey={5}>
+            Login/Register
+          </NavItem>
+        </LinkContainer>
       </Nav>
     );
 
@@ -39,13 +57,13 @@ class NavBar extends React.Component {
       <Navbar fixedTop inverse collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
-            <Link to="/"><img alt="Brand logo" src={Logo} /></Link>
+            <LinkContainer to="/"><img alt="Brand logo" src={Logo} /></LinkContainer>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
-          {inLinksLeft}
-          {inLinksRight}
+          {authed ? inLinksLeft : ''}
+          {authed ? logoutLink : loginLink}
         </Navbar.Collapse>
       </Navbar>
     );
