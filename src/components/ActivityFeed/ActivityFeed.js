f82a003ee835b11a4c6bec998bd0fa715c00efd8
John Achor <johnachor@gmail.com>
2018-08-07 16:35:37 -0500

    sending relevant comments to FriendReview and inserting original review text

--- src/components/ActivityFeed/ActivityFeed.js
+++ src/components/ActivityFeed/ActivityFeed.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import fbReviews from '../../firebaseReqs/queue';
+import fbComments from '../../firebaseReqs/comments';
 import FriendReview from '../FriendReview/FriendReview';
 import './ActivityFeed.css';
 
@@ -7,12 +8,13 @@ class ActivityFeed extends React.Component {
 
   state = {
     friendReviews: [],
+    comments: [],
   }
 
   getFriendActivities = () => {
-    fbReviews.getEntireQueue()
-      .then(queueItems => {
-        const friendReviews = Object.entries(queueItems.data)
+    Promise.all([fbReviews.getEntireQueue(), fbComments.getComments()])
+      .then(responseArray => {
+        const friendReviews = Object.entries(responseArray[0].data)
           // put firebase ID on objects
           .reduce((acc, kvp) => {
             kvp[1].firebaseId = kvp[0];
@@ -26,8 +28,9 @@ class ActivityFeed extends React.Component {
           // sort by date descending
           .sort((a, b) => { return b.reviewDate - a.reviewDate; })
           // limit to last 20 so we don't get a massive page that scrolls forever -- may include pagination later
-          .slice(0,20);
-        this.setState({ friendReviews: friendReviews });
+          .slice(0, 20);
+        const allComments = Object.values(responseArray[1].data);
+        this.setState({ friendReviews: friendReviews, comments: allComments });
       })
       .catch(err => console.error(err));
   }
@@ -39,8 +42,14 @@ class ActivityFeed extends React.Component {
   render() {
     const reviews = this.state.friendReviews.map(review => {
       const reviewer = this.props.friends.find(friend => { return friend.uid === review.ownerUid; });
+      const relevantComments = this.state.comments.filter(comment => {
+        return comment.reviewId === review.firebaseId;
+      })
+        .sort((a, b) => {
+          return a.commentDate - b.commentDate;
+        });
       return (
-        <FriendReview key={review.firebaseId} review={review} reviewer={reviewer}/>
+        <FriendReview key={review.firebaseId} review={review} reviewer={reviewer} comments={relevantComments}/>
       );
     });
 
--- src/components/FriendReview/FriendReview.js
+++ src/components/FriendReview/FriendReview.js
@@ -10,6 +10,7 @@ class FriendReview extends React.Component {
 
   state = {
     queued: false,
+    comments: [],
   }
 
   // revert queue object to initial unreviewed state, strip firebase ID, and change user to self, then add to queue
@@ -31,9 +32,25 @@ class FriendReview extends React.Component {
     this.props.history.push(`/details/${review.mediaType}/${review.mediaId}`);
   }
 
+  setComments = () => {
+    const comments = [...this.props.comments];
+    comments.unshift({
+      commentDate: 0,
+      commentText: this.props.review.reviewText,
+      commenterUid: this.props.review.ownerUid,
+      reviewId: this.props.review.firebaseId,
+    });
+    this.setState({ comments: comments });
+  }
+
+  componentWillReceiveProps() {
+    this.setComments();
+  }
+
   render() {
 
     const { review, reviewer } = this.props;
+
     return (
       <div className="reviewHolder">
         <div className="FriendReview col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
@@ -41,7 +58,7 @@ class FriendReview extends React.Component {
             <img src={`https://images.justwatch.com${review.posterUrl}`} alt="Poster" />
           </div>
           <div className="col-xs-8 col-sm-10 reviewBody">
-            <h4>{reviewer ? reviewer.username : ''} reviewed </h4>
+            <h4>{reviewer.username} reviewed </h4>
             <h4>{review.title}</h4>
             <StarRating name="reviewRating" starCount={5} value={review.starRating} editing={false} />
           </div>
@@ -52,13 +69,13 @@ class FriendReview extends React.Component {
         </div>
         <div className="commentHolder col-xs-12 col-sm-10 col-md-8">
           <div className="reviewComment col-xs-8 col-xs-offset-4 col-sm-10 col-sm-offset-2">
-            <p><b>{reviewer ? reviewer.username : ''}: </b>{review.reviewText}</p>
+            <p><b>{reviewer.username}: </b>{review.reviewText}</p>
           </div>
           <div className="reviewComment col-xs-8 col-xs-offset-4 col-sm-10 col-sm-offset-2">
-            <p><b>{reviewer ? reviewer.username : ''}: </b>{review.reviewText}</p>
+            <p><b>{reviewer.username}: </b>{review.reviewText}</p>
           </div>
           <div className="reviewComment col-xs-8 col-xs-offset-4 col-sm-10 col-sm-offset-2">
-            <p><b>{reviewer ? reviewer.username : ''}: </b>{review.reviewText}</p>
+            <p><b>{reviewer.username}: </b>{review.reviewText}</p>
           </div>
         </div>
       </div>
