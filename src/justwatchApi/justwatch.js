616f460ec8429de05b63dee580ec34c52ca08e03
John Achor <johnachor@gmail.com>
2018-08-14 15:07:09 -0500

    changed proxy endpoint again

--- src/justwatchApi/justwatch.js
+++ src/justwatchApi/justwatch.js
@@ -11,7 +11,7 @@ const jwGetItemDetail = (mediaType, mediaId) => {
 };
 
 const jwGetProviders = () => {
-  return axios.post(`${proxyDomain}/providers`, {});
+  return axios.get(`${proxyDomain}/providers`);
 };
 
 export default { jwSearch, jwGetItemDetail, jwGetProviders };
