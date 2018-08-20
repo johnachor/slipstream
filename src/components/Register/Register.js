e06d750cea5e10289e7a90456631987a210617a0
John Achor <johnachor@gmail.com>
2018-08-20 15:47:15 -0500

    registration validation is now much cleaner code

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -4,6 +4,7 @@ import { FormControl, ControlLabel, FormGroup, Button } from 'react-bootstrap';
 import fbAuth from '../../firebaseReqs/auth';
 import fbUsers from '../../firebaseReqs/users';
 import fbFriends from '../../firebaseReqs/friends';
+import { validateNewUser, getTestFailFeedback } from './validate';
 import './Register.css';
 
 class Register extends React.Component {
@@ -16,39 +17,16 @@ class Register extends React.Component {
     },
   };
 
-  // TODO: refactor this - single responsibility functions
   registerClickEvent = (e) => {
     const { user } = this.state;
     e.preventDefault();
     fbUsers.getAllUsers()
       .catch(err => console.error(err))
       .then(fbReturnedUsers => {
-        const existingUsernames = Object.values(fbReturnedUsers.data).reduce((usernameArray, user) => {
-          usernameArray.push(user.username.toLowerCase().split(' ').join(''));
-          return usernameArray;
-        }, []);
-        const existingEmails = Object.values(fbReturnedUsers.data).reduce((emailArray, user) => {
-          emailArray.push(user.email.toLowerCase().split(' ').join(''));
-          return emailArray;
-        }, []);
-        if (user.username.length < 6) {
-          alert('Please enter a username of at least 6 characters.');
-          document.getElementById('inputUsername').focus();
-        } else if (user.password.length < 6) {
-          alert('Passwords must be at least 6 characters in length.');
-        } else if (existingEmails.includes(user.email)) {
-          alert('Email address matches an existing account.');
-        } else if (existingUsernames.includes(user.username.toLowerCase().split(' ').join(''))) {
-          alert('Username already exists. Please choose another username.');
-        } else if (user.password !== user.confirmPassword) {
-          alert('Password fields do not match.');
-          const tempUser = { ...this.state.user };
-          tempUser.password = '';
-          tempUser.confirmPassword = '';
-          this.setState({ user: tempUser });
-          document.getElementById('inputPassword').value = '';
-          document.getElementById('confirmPassword').value = '';
-        } else {
+
+        const testResults = validateNewUser(user, fbReturnedUsers.data);
+
+        if (!testResults.length) {
           fbAuth.registerUser(user)
             .then((userResponse) => {
               const uid = userResponse.user.uid;
@@ -66,92 +44,94 @@ class Register extends React.Component {
               Promise.all([fbUsers.createUserObject(newUserObject), fbFriends.addRequest(newFriendObject)])
                 .then(this.props.history.push('/search'));
             }).catch(err => console.error(err));
+        } else {
+          alert('Please fix the following issues:\n\n' + testResults.map(getTestFailFeedback).join('\n'));
         }
       });
   };
 
-  emailChange = (e) => {
-    const tempUser = { ...this.state.user };
-    tempUser.email = e.target.value;
-    this.setState({ user: tempUser });
-  };
+emailChange = (e) => {
+  const tempUser = { ...this.state.user };
+  tempUser.email = e.target.value;
+  this.setState({ user: tempUser });
+};
 
-  passwordChange = (e) => {
-    const tempUser = { ...this.state.user };
-    tempUser.password = e.target.value;
-    this.setState({ user: tempUser });
-  };
+passwordChange = (e) => {
+  const tempUser = { ...this.state.user };
+  tempUser.password = e.target.value;
+  this.setState({ user: tempUser });
+};
 
-  confirmPasswordChange = (e) => {
-    const tempUser = { ...this.state.user };
-    tempUser.confirmPassword = e.target.value;
-    this.setState({ user: tempUser });
-  };
+confirmPasswordChange = (e) => {
+  const tempUser = { ...this.state.user };
+  tempUser.confirmPassword = e.target.value;
+  this.setState({ user: tempUser });
+};
 
-  usernameChange = (e) => {
-    const tempUser = { ...this.state.user };
-    tempUser.username = e.target.value;
-    this.setState({ user: tempUser });
-  };
+usernameChange = (e) => {
+  const tempUser = { ...this.state.user };
+  tempUser.username = e.target.value;
+  this.setState({ user: tempUser });
+};
 
-  render() {
-    const { user } = this.state;
-    return (
-      <div className="Register container">
-        <div id="register-form">
-          <form className="register-form col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
-            <h1 className="text-center">Register</h1>
-            <FormGroup>
-              <ControlLabel className="text-left">
-                Username
-              </ControlLabel>
-              <FormControl
-                type="text"
-                value={user.username}
-                placeholder="Username"
-                onChange={this.usernameChange}
-              />
-            </FormGroup>
-            <FormGroup>
-              <ControlLabel>
-                Email address
-              </ControlLabel>
-              <FormControl
-                type="text"
-                value={user.email}
-                placeholder="Email address"
-                onChange={this.emailChange}
-              />
-            </FormGroup>
-            <FormGroup>
-              <ControlLabel>
-                Password
-              </ControlLabel>
-              <FormControl
-                type="password"
-                value={user.password}
-                placeholder="Password"
-                onChange={this.passwordChange}
-              />
-            </FormGroup>
-            <FormGroup>
-              <ControlLabel>
-                Confirm Password
-              </ControlLabel>
-              <FormControl
-                type="password"
-                value={user.confirmPassword}
-                placeholder="Confirm Password"
-                onChange={this.confirmPasswordChange}
-              />
-            </FormGroup>
-            <Button bsStyle="primary" type="submit" onClick={this.registerClickEvent}>Register</Button>
-            <Link className="col-xs-12 text-right" to="/login">Looking for the login page?</Link>
-          </form>
-        </div>
+render() {
+  const { user } = this.state;
+  return (
+    <div className="Register container">
+      <div id="register-form">
+        <form className="register-form col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
+          <h1 className="text-center">Register</h1>
+          <FormGroup>
+            <ControlLabel className="text-left">
+              Username
+            </ControlLabel>
+            <FormControl
+              type="text"
+              value={user.username}
+              placeholder="Username"
+              onChange={this.usernameChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>
+              Email address
+            </ControlLabel>
+            <FormControl
+              type="text"
+              value={user.email}
+              placeholder="Email address"
+              onChange={this.emailChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>
+              Password
+            </ControlLabel>
+            <FormControl
+              type="password"
+              value={user.password}
+              placeholder="Password"
+              onChange={this.passwordChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>
+              Confirm Password
+            </ControlLabel>
+            <FormControl
+              type="password"
+              value={user.confirmPassword}
+              placeholder="Confirm Password"
+              onChange={this.confirmPasswordChange}
+            />
+          </FormGroup>
+          <Button bsStyle="primary" type="submit" onClick={this.registerClickEvent}>Register</Button>
+          <Link className="col-xs-12 text-right" to="/login">Looking for the login page?</Link>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
+}
 }
 
 export default Register;
---
+++ src/components/Register/validate.js
@@ -0,0 +1,67 @@
+const usernameIsLongEnough = (username) => {
+  return username.toLowerCase().split(' ').join('').length >= 6;
+};
+
+const passwordIsLongEnough = (password) => {
+  return password.length >= 6;
+};
+
+const emailIsCorrectlyFormatted = (email) => {
+  const emailRegEx = /\S+@\S+\.\S+/;
+  return emailRegEx.test(email);
+};
+
+const emailIsUnique = (email, existingEmails) => {
+  return !existingEmails.includes(email);
+};
+
+const usernameIsUnique = (username, existingUsernames) => {
+  return !existingUsernames.includes(username);
+};
+
+const passwordFieldsMatch = (password, confirmPassword) => {
+  return password === confirmPassword;
+};
+
+const validateNewUser = (newUser, usersArray) => {
+
+  const existingUsernames = Object.values(usersArray).reduce((usernameArray, user) => {
+    usernameArray.push(user.username.toLowerCase().split(' ').join(''));
+    return usernameArray;
+  }, []);
+
+  const existingEmails = Object.values(usersArray).reduce((emailArray, user) => {
+    emailArray.push(user.email.toLowerCase().split(' ').join(''));
+    return emailArray;
+  }, []);
+
+  const validationTests = [
+    usernameIsLongEnough(newUser.username),
+    passwordIsLongEnough(newUser.password),
+    emailIsCorrectlyFormatted(newUser.email),
+    emailIsUnique(newUser.email, existingEmails),
+    usernameIsUnique(newUser.username, existingUsernames),
+    passwordFieldsMatch(newUser.password, newUser.confirmPassword),
+  ];
+
+  return validationTests.reduce((failedIndices, currentTest, currentIndex) => {
+    if (currentTest !== true) failedIndices.push(currentIndex);
+    return failedIndices;
+  }, []);
+};
+
+const getTestFailFeedback = (testIndex) => {
+  const testFailMessages = [
+    'Username must be at least six characters (not including spaces).',
+    'Password must be at least six characters.',
+    'Email address not correctly formatted.',
+    'Email address is already registered.',
+    'Username is not unique.  Usernames are case and space insensitive.',
+    'Password fields do not match.',
+  ];
+
+  return testFailMessages[testIndex];
+};
+
+export { validateNewUser };
+export { getTestFailFeedback };
